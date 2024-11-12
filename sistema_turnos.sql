80d045ab211e783b7bc70931df12f4a541008ff4
lucasmanera-iso <lucas_emiliano_manera@hotmail.com>
2024-11-12 10:05:51 -0300

    Create sistema_turnos.sql

---
+++ sistema_turnos.sql
@@ -0,0 +1,240 @@
+-- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
+--
+-- Host: localhost    Database: sistema_turnos
+-- ------------------------------------------------------
+-- Server version	8.0.39
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cliente`
+--
+
+DROP TABLE IF EXISTS `cliente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cliente` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(30) NOT NULL,
+  `apellido` varchar(30) NOT NULL,
+  `telefono` varchar(20) NOT NULL,
+  `estado` int NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cliente`
+--
+
+LOCK TABLES `cliente` WRITE;
+/*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
+INSERT INTO `cliente` VALUES (1,'Nahiara','zaleski','3777111222',1),(2,'Lucas','Manera','3777111232',1),(3,'Juan','Gomez','3777676767',1);
+/*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `detalle_ticket`
+--
+
+DROP TABLE IF EXISTS `detalle_ticket`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `detalle_ticket` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_cliente` int DEFAULT NULL,
+  `id_personal` int DEFAULT NULL,
+  `id_servicios` int DEFAULT NULL,
+  `id_ticket` int DEFAULT NULL,
+  `id_precio` int DEFAULT NULL,
+  `subtotal` int DEFAULT NULL,
+  `estado` int NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  KEY `detalle_ticket_ibfk_6_idx` (`id_ticket`),
+  KEY `detalle_ticket_ibfk_2` (`id_cliente`),
+  KEY `detalle_ticket_ibfk_3` (`id_personal`),
+  KEY `detalle_ticket_ibfk_4` (`id_servicios`),
+  KEY `detalle_ticket_ibfk_5` (`id_precio`),
+  CONSTRAINT `detalle_ticket_ibfk_2` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id`),
+  CONSTRAINT `detalle_ticket_ibfk_3` FOREIGN KEY (`id_personal`) REFERENCES `personal` (`id`),
+  CONSTRAINT `detalle_ticket_ibfk_4` FOREIGN KEY (`id_servicios`) REFERENCES `servicios` (`id`),
+  CONSTRAINT `detalle_ticket_ibfk_5` FOREIGN KEY (`id_precio`) REFERENCES `precios` (`id`),
+  CONSTRAINT `detalle_ticket_ibfk_6` FOREIGN KEY (`id_ticket`) REFERENCES `ticket` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `detalle_ticket`
+--
+
+LOCK TABLES `detalle_ticket` WRITE;
+/*!40000 ALTER TABLE `detalle_ticket` DISABLE KEYS */;
+INSERT INTO `detalle_ticket` VALUES (1,3,2,4,1,1,7000,1);
+/*!40000 ALTER TABLE `detalle_ticket` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `personal`
+--
+
+DROP TABLE IF EXISTS `personal`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `personal` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(30) NOT NULL,
+  `apellido` varchar(30) NOT NULL,
+  `rol` varchar(20) NOT NULL,
+  `telefono` varchar(20) DEFAULT NULL,
+  `estado` int NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `personal`
+--
+
+LOCK TABLES `personal` WRITE;
+/*!40000 ALTER TABLE `personal` DISABLE KEYS */;
+INSERT INTO `personal` VALUES (1,'Mariana','Martinez','Peluquera','3777202020',1),(2,'Carlos','Alvarez','Barbero','3777222255',1),(3,'Simoni','Zaleski','Peluquera','3777151515',1),(4,'Keila','Zaleski','Maquillaje','3777898989',1),(5,'Nahiara','Zaleski','Uñas',NULL,1);
+/*!40000 ALTER TABLE `personal` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `precios`
+--
+
+DROP TABLE IF EXISTS `precios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `precios` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_servicios` int DEFAULT NULL,
+  `base` decimal(10,0) DEFAULT NULL,
+  `medida` enum('corto','medio','largo') DEFAULT NULL,
+  `duracion_min` int DEFAULT NULL,
+  `subtotal` decimal(10,0) DEFAULT NULL,
+  `estado` int NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  KEY `precios_ibfk_1_idx` (`id_servicios`),
+  CONSTRAINT `precios_ibfk_1` FOREIGN KEY (`id_servicios`) REFERENCES `servicios` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `precios`
+--
+
+LOCK TABLES `precios` WRITE;
+/*!40000 ALTER TABLE `precios` DISABLE KEYS */;
+INSERT INTO `precios` VALUES (1,4,6000,'medio',NULL,7000,1);
+/*!40000 ALTER TABLE `precios` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `servicios`
+--
+
+DROP TABLE IF EXISTS `servicios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `servicios` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `Rol` varchar(45) DEFAULT NULL,
+  `tipo` varchar(60) DEFAULT NULL,
+  `precio_base` decimal(10,0) DEFAULT NULL,
+  `estado` int NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `servicios`
+--
+
+LOCK TABLES `servicios` WRITE;
+/*!40000 ALTER TABLE `servicios` DISABLE KEYS */;
+INSERT INTO `servicios` VALUES (1,'Uñas','Softgel',7000,1),(2,'Uñas','Capping',6000,1),(3,'Uñas','semi',4000,1),(4,'Barberia','corte',6000,1),(5,'Peluqueria','Alisado',30000,1),(6,'Peluqueria','Color',20000,1);
+/*!40000 ALTER TABLE `servicios` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ticket`
+--
+
+DROP TABLE IF EXISTS `ticket`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ticket` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `fecha` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `total` decimal(10,0) DEFAULT NULL,
+  `estado` int NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ticket`
+--
+
+LOCK TABLES `ticket` WRITE;
+/*!40000 ALTER TABLE `ticket` DISABLE KEYS */;
+INSERT INTO `ticket` VALUES (1,'2024-11-12 02:04:37',NULL,1);
+/*!40000 ALTER TABLE `ticket` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `turno`
+--
+
+DROP TABLE IF EXISTS `turno`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `turno` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `fecha` timestamp NOT NULL,
+  `id_cliente` int NOT NULL,
+  `id_personal` int NOT NULL,
+  `id_servicios` int NOT NULL,
+  `estado` int NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  KEY `id_cliente` (`id_cliente`),
+  KEY `id_personal` (`id_personal`),
+  KEY `id_servicios` (`id_servicios`),
+  CONSTRAINT `turno_ibfk_1` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`id`),
+  CONSTRAINT `turno_ibfk_2` FOREIGN KEY (`id_personal`) REFERENCES `personal` (`id`),
+  CONSTRAINT `turno_ibfk_3` FOREIGN KEY (`id_servicios`) REFERENCES `servicios` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `turno`
+--
+
+LOCK TABLES `turno` WRITE;
+/*!40000 ALTER TABLE `turno` DISABLE KEYS */;
+INSERT INTO `turno` VALUES (1,'2024-11-15 20:00:00',2,5,2,1),(2,'2024-11-12 12:00:00',3,2,4,1);
+/*!40000 ALTER TABLE `turno` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-11-11 23:09:22
